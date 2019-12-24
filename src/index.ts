2a67c31d0998f92bc2cbdb026ffa3f261a444c26
Siggi Dünkel <chaosprinz76@googlemail.com>
2019-12-24 14:04:46 +0100

    adding basic bouncing ball example

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import "./styles.css"
 import P5 from "p5"
-import sketchMe from "./examples/wanderers"
+import sketchMe from "./examples/bouncingBall"
 
 const root = document.getElementById("app")
 

3764639c089aaf4924b75b39ec59a87418ca771b
chaosprinz76 <chaosprinz76@googlemail.com>
2019-12-29 08:55:05 +0100

    fixed divide to data-last-api, curried it and applied it in normalize

--- src/__tests__/pvector/divide.ts
+++ src/__tests__/pvector/divide.ts
@@ -1,15 +1,15 @@
 import { pVector, divide } from '../../lib/pvector'
 
 describe("divide - divide a vector by a given number", () => {
-  const arg1 = pVector(30, 50)
-  const arg2 = 2
+  const arg1 = 2
+  const arg2 = pVector(30, 50)
   const result = divide(arg1, arg2)
 
-  test('it should have arg1s x divided by arg2', () => {
-    expect(result.x).toEqual(arg1.x / arg2)
+  test('it should have arg2s x divided by arg1', () => {
+    expect(result.x).toEqual(arg2.x / arg1)
   })
 
-  test('it should have arg1s y divided by arg2', () => {
-    expect(result.y).toEqual(arg1.y / arg2)
+  test('it should have arg2s y divided by arg1', () => {
+    expect(result.y).toEqual(arg2.y / arg1)
   })
 })
--- src/lib/pvector/normalize.ts
+++ src/lib/pvector/normalize.ts
@@ -1,9 +1,9 @@
 import { PVector } from "../../types";
 import divide from "./divide";
 import magnitude from "./magnitude";
+import { compose } from "ramda";
 
 export const normalize = (vector: PVector): PVector =>
-  divide(vector, magnitude(vector))
-
+  divide(magnitude(vector), vector)
 
 export default normalize
